5ef32425eaff732998efe6c10fb3fed618ba3ac3
Isaiah <52585456+Isaiahdj@users.noreply.github.com>
2019-07-16 13:56:31 -0700

    Html project

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("hello world")
+function My_First_Function() {
+		var string = "This is a snow husky with blue eyes";
+		var result = string.fontcolor("blue");
+		document.getElementById("Husky").innerHTML =
+		result;
+	}
